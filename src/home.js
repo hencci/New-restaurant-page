59faff4cc1bd1c52c1001c9c463861a08e364b09
hencci <hencci20@gmail.com>
2025-03-13 16:19:34 +0100

    Move import to top

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import icecream1 from "./images/icecream1.jpg";
+
 export default function loadHome() {
     const content = document.getElementById("content");
     content.innerHTML = ""; // Clear previous content
@@ -10,7 +12,6 @@ export default function loadHome() {
     const description = document.createElement("p");
     description.textContent = "Serving the best icecream since 2010";
 
-    import icecream1 from "./images/icecream1.jpg";
     const homeImage = document.createElement("img");
     homeImage.src = icecream1;
     homeImage.alt = "Icecream Image";
