803bcf42c5470844d858434b9e7596916fa15c86
hencci <hencci20@gmail.com>
2025-03-13 15:51:36 +0100

    Create image

--- src/home.js
+++ src/home.js
@@ -5,12 +5,18 @@ export default function loadHome() {
     const homeDiv = document.createElement("div");
 
     const headline = document.createElement("h1");
-    headline.textContent = "elcome to Our Restaurant";
+    headline.textContent = "Welcome to Our Restaurant";
 
     const description = document.createElement("p");
     description.textContent = "Serving the best icecream since 2010";
 
+    import icecream1 from "./images/icecream1.jpg";
+    const homeImage = document.createElement("img");
+    homeImage.src = icecream1;
+    homeImage.alt = "Icecream Image";
+
     homeDiv.appendChild(headline);
     homeDiv.appendChild(description);
+    homeDiv.appendChild(homeImage);
     content.appendChild(homeDiv);
 }
