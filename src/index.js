871332a3b36c8d4edc6d11784d7c89cf87305c54
hencci <hencci20@gmail.com>
2025-03-15 10:47:55 +0100

    Create function to handle nav click events

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import loadHome from "./home";
 import loadMenu from "./menu";
 
+function addNavEventListeners() {
+    document.getElementById("home-btn").addEventListener("click", loadHome);
+    document.getElementById("menu-btn").addEventListener("click", loadMenu);
+}
+
 function initWebsite() {
     loadHome();
+    addNavEventListeners(); //Call function
 }
 
 initWebsite();
