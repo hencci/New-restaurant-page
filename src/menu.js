dc4c8e8297eec2cda4b2c45d059ac590377eb738
hencci <hencci20@gmail.com>
2025-03-18 09:46:36 +0100

    Add more items on the menu

--- src/menu.js
+++ src/menu.js
@@ -24,9 +24,55 @@ export default function loadMenu() {
     const itemText1 = document.createElement("p");
     itemText1.textContent = "vanilla - $5";
 
+    const itemDiv2 = document.createElement("div");
+    itemDiv2.classList.add("item");
+    const itemImage2 = document.createElement("img");
+    itemImage2.src = chocolate;
+    itemImage2.alt = "Chocolate icecream";
+    const itemText2 = document.createElement("p");
+    itemText2.textContent = "chocolate - $7";
+
+    const itemDiv3 = document.createElement("div");
+    itemDiv3.classList.add("item");
+    const itemImage3 = document.createElement("img");
+    itemImage3.src = strawberry;
+    itemImage3.alt = "Strawberry icecream";
+    const itemText3 = document.createElement("p");
+    itemText3.textContent = "strawberry - $7";
+
+    const itemDiv4 = document.createElement("div");
+    itemDiv4.classList.add("item");
+    const itemImage4 = document.createElement("img");
+    itemImage4.src = cookiesAndCream;
+    itemImage4.alt = "Cookies and Cream";
+    const itemText4 = document.createElement("p");
+    itemText4.textContent = "cookies and cream - $6";
+
+    const itemDiv5 = document.createElement("div");
+    itemDiv5.classList.add("item");
+    const itemImage5 = document.createElement("img");
+    itemImage5.src = mintChocolateChips;
+    itemImage5.alt = "Mint Chocolate Chips";
+    const itemText5 = document.createElement("p");
+    itemText5.textContent = "mint chocolate chips - $6";
+
     itemDiv1.appendChild(itemImage1);
     itemDiv1.appendChild(itemText1);
+    itemDiv2.appendChild(itemImage2);
+    itemDiv2.appendChild(itemText2);
+    itemDiv3.appendChild(itemImage3);
+    itemDiv3.appendChild(itemText3);
+    itemDiv4.appendChild(itemImage4);
+    itemDiv4.appendChild(itemText4);
+    itemDiv5.appendChild(itemImage5);
+    itemDiv5.appendChild(itemText5);
+
     menuListDiv.appendChild(itemDiv1);
+    menuListDiv.appendChild(itemDiv2);
+    menuListDiv.appendChild(itemDiv3);
+    menuListDiv.appendChild(itemDiv4);
+    menuListDiv.appendChild(itemDiv5);
+
     menuDiv.appendChild(title);
     menuDiv.appendChild(menuListDiv);
     content.appendChild(menuDiv);
