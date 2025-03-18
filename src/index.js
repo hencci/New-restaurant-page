7dce690ba970cc4bb5edb64499c150126a8766e5
hencci <hencci20@gmail.com>
2025-03-18 10:09:27 +0100

    Import loadContact, and add click event

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import loadHome from "./home";
 import loadMenu from "./menu";
+import loadContact from "./contact";
 
 function addNavEventListeners() {
     document.getElementById("home-btn").addEventListener("click", loadHome);
     document.getElementById("menu-btn").addEventListener("click", loadMenu);
+    document.getElementById("contact-btn").addEventListener("click", loadContact);
 }
 
 function initWebsite() {
